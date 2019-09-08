52c14f913ed171508e0e409904b0ec2f793e941a
ram <ram@ram-PC>
2019-09-08 12:15:03 +0530

    dgfgd

---
+++ DoctorManagementSystem/src/main/java/com/AlightHub/DMS/ServiceImpl/LoginServiceImple.java
@@ -0,0 +1,16 @@
+package com.AlightHub.DMS.ServiceImpl;
+
+import com.AlightHub.DMS.Model.Login;
+import com.AlightHub.DMS.Service.LoginService;
+
+public class LoginServiceImple implements LoginService {
+
+	@Override
+	public Login loginCheck() {
+		System.out.println("jhfjhksf");
+		
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
