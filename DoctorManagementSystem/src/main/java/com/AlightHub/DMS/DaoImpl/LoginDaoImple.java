b5e0ce5dd6b78023908db84b45bbbf395a7e83c1
ram <ram@ram-PC>
2019-09-08 13:18:52 +0530

    simple demo

---
+++ DoctorManagementSystem/src/main/java/com/AlightHub/DMS/DaoImpl/LoginDaoImple.java
@@ -0,0 +1,17 @@
+package com.AlightHub.DMS.DaoImpl;
+
+import com.AlightHub.DMS.Dao.LoginDao;
+import com.AlightHub.DMS.Model.Login;
+
+public class LoginDaoImple implements LoginDao {
+
+	@Override
+	public Login loginCheck() {
+		System.out.println(" welcome to doctor projects...........");
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+}
