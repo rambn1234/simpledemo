581c2fe1d596a4df7a13130041aa8a6c08b368c0
ram <ram@ram-PC>
2019-09-08 11:37:54 +0530

    doctor project

---
+++ SpringJpaDropdounlist/src/main/java/com/cjc/SpringJpaDropdounlistApplication.java
@@ -0,0 +1,13 @@
+package com.cjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaDropdounlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaDropdounlistApplication.class, args);
+	}
+
+}
